e2ad52e4e9100f35425519a77c19c616c83f1865
Harun Bekcan <harunbekcan@gmail.com>
2023-11-15 19:32:06 +0300

    RickAndMortyResponse created.

---
+++ app/src/main/java/com/harunbekcan/rickandmortyapp/data/response/Location.kt
@@ -0,0 +1,6 @@
+package com.harunbekcan.rickandmortyapp.data.response
+
+data class Location(
+    val name: String?,
+    val url: String?
+)
---
+++ app/src/main/java/com/harunbekcan/rickandmortyapp/data/response/Origin.kt
@@ -0,0 +1,6 @@
+package com.harunbekcan.rickandmortyapp.data.response
+
+data class Origin(
+    val name: String?,
+    val url: String?
+)
