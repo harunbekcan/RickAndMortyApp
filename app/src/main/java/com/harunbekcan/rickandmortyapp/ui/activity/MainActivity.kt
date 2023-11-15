9fc0dafaf2d09dc315488b465269a45ef9e1547f
Harun Bekcan <harunbekcan@gmail.com>
2023-11-15 17:30:27 +0300

    CharactersFragment created.

--- app/src/main/java/com/harunbekcan/rickandmortyapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/harunbekcan/rickandmortyapp/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.harunbekcan.rickandmortyapp
+package com.harunbekcan.rickandmortyapp.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.harunbekcan.rickandmortyapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/harunbekcan/rickandmortyapp/ui/fragment/CharactersFragment.kt
@@ -0,0 +1,11 @@
+package com.harunbekcan.rickandmortyapp.ui.fragment
+
+import android.os.Bundle
+import com.harunbekcan.rickandmortyapp.base.BaseFragment
+import com.harunbekcan.rickandmortyapp.databinding.FragmentCharactersBinding
+
+class CharactersFragment : BaseFragment<FragmentCharactersBinding>(FragmentCharactersBinding::inflate) {
+    override fun prepareView(savedInstanceState: Bundle?) {
+
+    }
+}
