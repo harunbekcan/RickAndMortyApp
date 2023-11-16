af9c6fa377e2ffb66afba61eca8290c34697ab7e
Harun Bekcan <harunbekcan@gmail.com>
2023-11-17 01:03:44 +0300

    BaseFragment removed.

--- app/src/main/java/com/harunbekcan/rickandmortyapp/ui/fragment/CharactersFragment.kt
+++ app/src/main/java/com/harunbekcan/rickandmortyapp/ui/fragment/CharactersFragment.kt
@@ -1,28 +1,40 @@
 package com.harunbekcan.rickandmortyapp.ui.fragment
 
 import android.os.Bundle
+import android.view.View
 import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.paging.LoadState
-import com.harunbekcan.rickandmortyapp.base.BaseFragment
+import com.harunbekcan.rickandmortyapp.R
 import com.harunbekcan.rickandmortyapp.databinding.FragmentCharactersBinding
 import com.harunbekcan.rickandmortyapp.ui.adapter.CharactersPagingAdapter
 import com.harunbekcan.rickandmortyapp.utils.collectFlow
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class CharactersFragment : BaseFragment<FragmentCharactersBinding>(FragmentCharactersBinding::inflate) {
+class CharactersFragment : Fragment(R.layout.fragment_characters) {
 
-    private lateinit var charactersPagingAdapter: CharactersPagingAdapter
+    private var _binding : FragmentCharactersBinding ?= null
+    private val binding get() = _binding!!
+
+    private val charactersPagingAdapter = CharactersPagingAdapter()
     private val viewModel: CharactersViewModel by viewModels()
-    override fun prepareView(savedInstanceState: Bundle?) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentCharactersBinding.bind(view)
         initPagingAdapter()
         initProgressBar()
         pagingDataObserve()
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
     private fun initPagingAdapter() {
-        charactersPagingAdapter = CharactersPagingAdapter()
         binding.charactersRecyclerView.adapter = charactersPagingAdapter
     }
 
